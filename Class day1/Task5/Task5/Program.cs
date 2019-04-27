dc18b96c00c68696a5f90cc137dac879a72858d2
Justina <justina.bauziene@gmail.com>
2019-04-27 20:13:39 +0300

    Task5
    Initial commit

---
+++ Class day1/Task5/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            double div = (double) number1 / (double) number2;
+            Console.WriteLine("Dvieju skaiciu suma {0}", number1 + number2);
+            Console.WriteLine("Dvieju skaiciu skirtumas {0}", Math.Abs(number1 - number2));
+            Console.WriteLine("Dvieju skaiciu daugyba {0}", number1 * number2);
+            Console.WriteLine("Dvieju skaiciu dalyba {0}", div);
+        }
+    }
+}
