5568a8a541d229f29dca0fa2d3951a82845c0134
Justina <justina.bauziene@gmail.com>
2019-04-27 19:46:40 +0300

    Task4 completed
    Initial commit

---
+++ Class day1/Task4/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Atsakymas -6+3*5: {0}", -6+3*5);
+            Console.WriteLine("Atsakymas (13-2)*7: {0}", (13 - 2) * 7);
+            Console.WriteLine("Atsakymas (15-2)+(20/10): {0}", (15 - 2) + (20 / 10));
+        }
+    }
+}
