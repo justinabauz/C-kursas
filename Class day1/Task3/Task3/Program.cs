ef0a8ff99515c24803da1786bbf60131dd86d206
Justina <justina.bauziene@gmail.com>
2019-04-27 19:37:18 +0300

    Task3 completed
    Initial commit

---
+++ Class day1/Task3/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double number1 = 24;
+            double number2 = 5;
+            Console.WriteLine(number1 / number2);
+        }
+    }
+}
