23e30be1482655a85a8952a1e9f919aaae7173cb
Justina <justina.bauziene@gmail.com>
2019-04-27 21:18:42 +0300

    Task7 completed
    Initial commit

---
+++ Class day1/Task7/Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Task7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double[] array = new double[4];
+            Console.WriteLine("Iveskite 4 skaicius:");
+            for (int i = 0; i < 4; i++)
+            {
+                array[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine(array.Average());
+        }
+    }
+}
