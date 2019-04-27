1786429ca0f851ea437c1ed42949aa564f3a2259
Justina <justina.bauziene@gmail.com>
2019-04-27 20:40:51 +0300

    Task6 completed
    Initial commit

---
+++ Class day1/Task6/Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Daugybos lentele yra:");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("{0}*{1}={2}", i, number, i * number);
+            }
+        }
+    }
+}
