edccdb15b4682d20f3181badfc0b109ebe9d42b5
Justina <justina.bauziene@gmail.com>
2019-04-27 19:30:58 +0300

    Task2 completed
    Initial commit

---
+++ Class day1/Task2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number1 = 17;
+            int number2 = 23;
+            Console.WriteLine("Antros uzduoties atsakymas: {0}", number1 + number2);
+        }
+    }
+}
