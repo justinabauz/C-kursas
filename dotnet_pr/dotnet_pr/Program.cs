cd3750de40f2f7897599c7569f52131acaefd56e
Justina <justina.bauziene@gmail.com>
2019-04-27 17:05:09 +0300

    initial_project_commit
    initial

---
+++ dotnet_pr/dotnet_pr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_pr
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
