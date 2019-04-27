01c16d24683aa4815578b82e9fa99686241d5dab
Justina <justina.bauziene@gmail.com>
2019-04-27 19:20:18 +0300

    task 1 completed
    Initial commit

---
+++ Class day1/Task1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Kokia tavo megstamiausia spalva?");
+            var user_colour = Console.ReadLine();
+            Console.WriteLine("Mano irgi " + user_colour);
+        }
+    }
+}
