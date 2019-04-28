1dd894f8a0082ee9a2b75ae3c9790f9b0db73a77
Justina <justina.bauziene@gmail.com>
2019-04-28 09:02:23 +0300

    Task10 completed
    Initial commit

---
+++ Class day1/Task10/Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Irasykite Celsijaus laipsnius:");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+            double fahrenheit = (double) celsius * 18 / 10 + 32;
+            Console.WriteLine("Kelvin: {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit: {0}", fahrenheit);
+        }
+    }
+}
