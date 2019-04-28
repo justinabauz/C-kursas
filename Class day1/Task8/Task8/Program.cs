de62185c94f1d6cc370f1a3327c32b9f10fbef33
Justina <justina.bauziene@gmail.com>
2019-04-28 08:31:20 +0300

    Task8 completed
    Initial commit

--- Class day1/Task8/Task8/Program.cs
+++ Class day1/Task8/Task8/Program.cs
@@ -6,7 +6,16 @@ namespace Task8
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Ramai!");
+            Console.WriteLine("Iveskite 3 skaicius (A,B,C):");
+            string numberStr = Console.ReadLine();
+            string[] splitted = numberStr.Split(',');
+            int[] nums = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                nums[i] = int.Parse(splitted[i]);
+            }
+            Console.WriteLine("Atlikus skaiciavimus (A+B)*C gauname: {0}", (nums[0]+nums[1])*nums[2]);
+            Console.WriteLine("Atlikus skaiciavimus A*C+B*C gauname: {0}", nums[0] *  nums[2] + nums[1]*nums[2]);
         }
     }
 }
