72531853713d95210404a4399836a67870b8787d
Justina <justina.bauziene@gmail.com>
2019-04-28 08:50:24 +0300

    Task9 completed
    Initial commit

---
+++ Class day1/Task9/Task9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite simboli!");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("Kvadratas");
+            Console.WriteLine("{0}{0}{0}",symbol);
+            Console.WriteLine("{0} {0}", symbol);
+            Console.WriteLine("{0} {0}", symbol);
+            Console.WriteLine("{0} {0}", symbol);
+            Console.WriteLine("{0}{0}{0}", symbol);
+        }
+    }
+}
