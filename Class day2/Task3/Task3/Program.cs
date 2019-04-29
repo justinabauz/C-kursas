34b5f5e426c932a19656f827528710d04c4183d1
Justina <justina.bauziene@gmail.com>
2019-04-29 19:40:57 +0300

    Task3 completed
    Initial commit

---
+++ Class day2/Task3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string school = "Vilniaus licejus";
+            string course = "IT";
+            int numberOfstudents = 100;
+            string date = DateTime.Now.ToString("d/MM/yyyy");
+            Console.WriteLine("Mokykla - {0}, kursas - {1}, studentu skaicius - {2} ir laikas - {3}", school, course, numberOfstudents, date);
+        }
+    }
+}
