5ea487ede8751fe99ead56378c72050174395d8c
Justina <justina.bauziene@gmail.com>
2019-04-29 21:58:55 +0300

    Ola!

---
+++ Class day2/Op9/Op9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Op9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string town = "Vilniusl";
+            string street = "Linkmenu str";
+            int str_number = 34;
+            int building_year = 2017;
+            Console.WriteLine("Miestas: {0}, Gatve: {1}, Namo numeris: {2}, Statymo metai {3}", town, street, str_number, building_year);
+        }
+    }
+}
