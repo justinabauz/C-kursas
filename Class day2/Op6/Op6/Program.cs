cdadfcc9a8e1fe8a89138d9143c71809c3090db3
Justina <justina.bauziene@gmail.com>
2019-04-29 21:04:08 +0300

    Op6 completed
    commit

---
+++ Class day2/Op6/Op6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Op6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite teksta:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Ar ivesta eilute tuscia? {0}", text=="");
+        }
+    }
+}
