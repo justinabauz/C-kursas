d2f33fcb0577666fcd01a499f80f930289527532
Justina <justina.bauziene@gmail.com>
2019-04-29 21:06:38 +0300

    Op7 completed
    commit

---
+++ Class day2/Op7/Op7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Op7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaciu:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ar skaicius mazesnis uz 0? {0}", number < 0);
+        }
+    }
+}
