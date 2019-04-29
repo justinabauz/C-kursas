d2077f6cf6cc1ff5205f1b53139edcefd29c42c7
Justina <justina.bauziene@gmail.com>
2019-04-29 20:40:57 +0300

    TaskOp2 completed
    Initial commit

---
+++ Class day2/Operation2/Operation2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operation2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 15;
+            int C = A;
+            Console.WriteLine("Pirminis varianats: A = {0}, B= {1},", A, B);
+            Console.WriteLine("Pirminis varianats: A = {0}, B = {1} ", A=B, B=C);
+        }
+    }
+}
+
