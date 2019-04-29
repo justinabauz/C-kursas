9f07c2199bf61977c5c2955abaada66369b5b3db
Justina <justina.bauziene@gmail.com>
2019-04-29 20:55:10 +0300

    TaskOp4 completed
    commit

---
+++ Class day2/Op4/Op4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Op4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirma skaiciu:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pirma skaiciu padidinam vienu - {0}, antra skaiciu sumazinam vienu - {1}", ++number1, --number2);
+        }
+    }
+}
