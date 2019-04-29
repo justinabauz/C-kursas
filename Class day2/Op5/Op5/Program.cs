cc42ca187e730aff85182d624dfdfd5f25545659
Justina <justina.bauziene@gmail.com>
2019-04-29 21:00:59 +0300

    Op5 completed
    commit

---
+++ Class day2/Op5/Op5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Op5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite staciakampio ilgi:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite staciakampio ploti:");
+            int hight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Staciakampio plotas:{0}", width*hight);
+        }
+    }
+}
