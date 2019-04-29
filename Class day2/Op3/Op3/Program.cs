e70d6162d543dff575a67671e097150263b25d77
Justina <justina.bauziene@gmail.com>
2019-04-29 20:46:31 +0300

    Op3 completed
    commit

---
+++ Class day2/Op3/Op3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Op3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirma skaiciu:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ar skaiciai lygus? {0}", number1==number2);
+        }
+    }
+}
