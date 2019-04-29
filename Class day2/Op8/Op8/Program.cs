672ef794f2dd76a26d1be5f43a06dbb2675e7d35
Justina <justina.bauziene@gmail.com>
2019-04-29 21:11:55 +0300

    Op8 completed
    commit

---
+++ Class day2/Op8/Op8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Op8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite varda:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Iveskite pavarde:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Vardar ir Pavarde yra: {0}", name+" "+surname);
+        } 
+    }
+}
