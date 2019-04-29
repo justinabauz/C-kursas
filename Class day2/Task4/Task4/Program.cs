5aca74710cc8b4eb41d1c3e94b18ffea4970a8f3
Justina <justina.bauziene@gmail.com>
2019-04-29 20:20:43 +0300

    Task4 completed
    Initial commit

---
+++ Class day2/Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        { 
+            DateTime StartDate = new DateTime(2019, 4, 15);
+            DateTime EndDate = new DateTime(2019, 4, 28);
+            double numberOfdays = (EndDate - StartDate).TotalDays;
+            double numberOfweeks = (EndDate - StartDate).TotalDays / 7;
+            var totalDays = 0;
+            for (var date = StartDate; date < EndDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek != DayOfWeek.Saturday
+                    && date.DayOfWeek != DayOfWeek.Sunday
+                    && date.DayOfWeek != DayOfWeek.Friday)
+                    totalDays++;
+            }
+            Console.WriteLine("Diff between days without weekends and Friday: {0}", totalDays);
+        }
+    }
+
+}
