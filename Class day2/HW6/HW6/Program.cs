fd6d538245c804bf9a4b18f7a4765449bc009f8e
Justina <justina.bauziene@gmail.com>
2019-04-30 19:04:15 +0300

    HW6 completed
    +

---
+++ Class day2/HW6/HW6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 15;
+            Console.WriteLine("Pradines reiksmes A = {0}, B = {1}: ", A, B);
+            (A, B) = (B, A);
+            Console.WriteLine("Sukeitus reiksmes gauname: A = {0}, B = {1}: ", A, B);
+        }
+    }
+}
