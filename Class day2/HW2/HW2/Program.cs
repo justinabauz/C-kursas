06445254c4770673df8170641e7da3a10d248c24
Justina <justina.bauziene@gmail.com>
2019-04-30 18:31:58 +0300

    HW2 completed
    +

---
+++ Class day2/HW2/HW2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite apskritimo spinduli");
+            int spindulys = Convert.ToInt32(Console.ReadLine());
+            double ilgis = 2 * spindulys * Math.PI;
+            double plotas = Math.PI * spindulys * spindulys;
+            Console.WriteLine("Apskritimo ilgis yra - {0}, o plotas {1}", ilgis, plotas);
+        }
+    }
+}
