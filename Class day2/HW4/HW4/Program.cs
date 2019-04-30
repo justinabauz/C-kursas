91cf738058c72ad959743d44d62c02fc3c304036
Justina <justina.bauziene@gmail.com>
2019-04-30 18:51:55 +0300

    HW4 completed
    +

---
+++ Class day2/HW4/HW4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace HW4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu (sekundes)");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            string str = time.ToString(@"hh\:mm\:ss");
+            Console.WriteLine("Pavertus i valandas, minutes, sekundes gauname: {0}", str);
+        }
+    }
+}
