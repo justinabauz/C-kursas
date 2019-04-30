3bfcb3136ff3c9e752f83a65b27dfbe4c10f59f6
Justina <justina.bauziene@gmail.com>
2019-04-30 18:43:06 +0300

    HW3 completed
    +

---
+++ Class day2/HW3/HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite darbuotojo ID:");
+            string ID = Console.ReadLine();
+            Console.WriteLine("Iveskite darbuotojo isdirbtas valandas");
+            int valandos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite darbuotojo valandini ikaini");
+            int ikainis = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Darbuotojo ID: {0}", ID);
+            Console.WriteLine("Darbuotojo alga: {0}", valandos * ikainis);
+        }
+    }
+}
