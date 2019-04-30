6a0b0eb49a26bd661fda22b8d3389a9a6f71635d
Justina <justina.bauziene@gmail.com>
2019-04-30 19:14:55 +0300

    HW7 completed
    +

---
+++ Class day2/HW7/HW7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite dvieju skaitmenu skaiciu:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Skaitmenu suma: {0}+{1}={2}", number/10, number%10, number /10 + number % 10);
+
+        }
+    }
+}
