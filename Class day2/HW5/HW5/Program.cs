83ff96275b4b7068c836daa676d4998586ec3c4f
Justina <justina.bauziene@gmail.com>
2019-05-01 11:41:31 +0300

    completed

--- Class day2/HW5/HW5/Program.cs
+++ Class day2/HW5/HW5/Program.cs
@@ -10,7 +10,12 @@ namespace HW5
             int x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Iveskite staciakampio krastines y ilgi:");
             int y = Convert.ToInt32(Console.ReadLine());
-            int 
+            int count = 0;
+            if (x >= 2 && y >= 2)
+            {
+                count = x * y / 6;
+            }
+            Console.WriteLine("Ar galima ibrezti staciakampi 2*3 i staciakampi {0}*{1}? Atsakymas {2}:", x,y, count);
         }
     }
 }
