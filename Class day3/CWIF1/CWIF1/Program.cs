a5a9176a80804744c24b39fed01b1fb966245cb3
Justina <justina.bauziene@gmail.com>
2019-05-02 21:15:09 +0300

    +

---
+++ Class day3/CWIF1/CWIF1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CWIF1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite grupes pavadinima:");
+            string grupesPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite grupes nariu skaiciu:");
+            //int grupesNariuskaicius = int.Parse(Console.ReadLine());
+            int nariuSkaicius;
+            if (!int.TryParse(Console.ReadLine(), out nariuSkaicius))// rezultatas True or False
+            {
+
+                Console.WriteLine("Ivestas blogas skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Grupes pavadinimas yra:{0}, nariu skaicius yra {1}:", grupesPavadinimas, nariuSkaicius);
+            Console.ReadLine();
+
+        }
+    }
+}
