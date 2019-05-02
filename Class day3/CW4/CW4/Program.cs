d8068e25e06ddc2937780b464e8a23d58562adda
Justina <justina.bauziene@gmail.com>
2019-05-02 19:33:00 +0300

    +

---
+++ Class day3/CW4/CW4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CW4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            sbyte a = 126;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.ReadLine(); 
+        }
+    }
+}
