47cc29ef33ef21fa634fa4db3712db7404f2e3ca
Justina <justina.bauziene@gmail.com>
2019-05-02 19:53:30 +0300

    +

---
+++ Class day3/CW5/CW5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CW5
+{
+    class Human
+    {
+        public string Name; // sukurem tipa
+    }
+
+        class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Human human1 = new Human(); // tokiu budu apsiraso naujo tipo aprasymas, su sudetiniais tipais 
+            human1.Name = "Batman";
+
+            Human human2 = human1;
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            human1.Name = "Joker";
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
