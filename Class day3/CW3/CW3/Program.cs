3f6a0d1fe70c41c08180a157f6e5cf3b86852525
Justina <justina.bauziene@gmail.com>
2019-05-02 19:13:19 +0300

    completed

---
+++ Class day3/CW3/CW3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CW3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            byte a = 0;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a = 254;
+            a++;
+
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            Console.ReadLine();//perpildo byte tipa, jame telpa tik 255, grazina 0
+
+        }
+    }
+}
