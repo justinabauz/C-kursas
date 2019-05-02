b7e5de8bd37ae7d0220b8bf824a3c085ae434687
Justina <justina.bauziene@gmail.com>
2019-05-02 21:32:17 +0300

    +

---
+++ Class day3/CWIF2/CWIF2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CWIF2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Koki veiksma norite atlikti?");
+            Console.WriteLine("[1] - istrinti vartotoja, [2] - sukurti vartotoja, [3] - redaguoti esama?");
+
+            int veiksmas = int.Parse(Console.ReadLine());
+            switch (veiksmas)
+            {
+                case 1:
+                    Console.WriteLine("Vartotojas sekmingai istrintas...");
+                    break;
+                case 2:
+                    Console.WriteLine("Vartotojas sekmingai sukurtas...");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Vartotojo redagavimo rezimas");
+                    break;
+
+                default:
+                    Console.WriteLine("Nezinomas veiksmas. Paspauskite [ENTER], kad uzdarytumet langa.");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+
+            }
+            Console.WriteLine("Paspauskite [ENTER], kad uzdarytumet langa.");
+            Console.ReadLine();
+
+        }
+    }
+}
