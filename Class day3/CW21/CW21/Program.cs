a7010207a4b1dd5438adb2ebbd5a56fdf817e3ec
Justina <justina.bauziene@gmail.com>
2019-05-02 20:10:09 +0300

    +

---
+++ Class day3/CW21/CW21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CW21
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite simboli:");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("{0}", symbol);
+            Console.WriteLine("{0}{0}", symbol);
+            Console.WriteLine("{0}{0}{0}", symbol);
+        }
+    }
+}
