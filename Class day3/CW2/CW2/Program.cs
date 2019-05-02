22262bec7c34f04980df78cc8844cc270299801f
Justina <justina.bauziene@gmail.com>
2019-05-02 19:08:35 +0300

    completed

---
+++ Class day3/CW2/CW2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CW2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double skaicius1 = 0.1 * 10; //Rasom decimal vietoj double bus lygu
+            double skaicius2 = 0;
+
+            for (int ctr = 0; ctr < 10; ctr++)
+            {
+                skaicius2 += 0.1; //0.1m parodo decimal
+            }
+            Console.WriteLine("{0:R} = {1:R} : {2}", skaicius1, skaicius2, skaicius1 == skaicius2); //Vioetoj R rasom N
+            Console.ReadLine();
+
+        }
+    }
+}
