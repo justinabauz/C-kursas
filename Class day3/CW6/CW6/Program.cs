36133560a0af91525280572df91a879301bf7c9e
Justina <justina.bauziene@gmail.com>
2019-05-02 20:02:19 +0300

    +

---
+++ Class day3/CW6/CW6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CW6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double x = 1234.7;
+            int a;
+                        
+            //a=x;
+            a = (int)x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
