073b394d340de28ff3488b3402f4bed8f9ebb9d7
Justina <justina.bauziene@gmail.com>
2019-05-02 18:53:49 +0300

    completed
    +

--- Class day2/HW8/HW8/Program.cs
+++ Class day2/HW8/HW8/Program.cs
@@ -20,7 +20,7 @@ namespace HW8
             int[] nums2 = new int[2];
             for (int i = 0; i < 2; i++)
             {
-                nums2[i] = int.Parse(splitted[i]);
+                nums2[i] = int.Parse(splitted2[i]);
             }
             Console.WriteLine("Iveskite treciojo tasko koordinates:(M,N)");
             string point3 = Console.ReadLine();
@@ -28,7 +28,7 @@ namespace HW8
             int[] nums3 = new int[2];
             for (int i = 0; i < 2; i++)
             {
-                nums3[i] = int.Parse(splitted[i]);
+                nums3[i] = int.Parse(splitted3[i]);
             }
             Console.WriteLine("Ar taskas (M,N) yra staciakampio viduje? {0}", nums1[0] <= nums3[0] && nums2[0] >= nums3[0] && nums1[1] <= nums3[1] && nums2[1] >= nums3[1]);
         }
---
+++ Class day3/CW1/CW1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CW1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            float skaiciusFloat = 1f / 3;
+            double skaiciusDouble = 1d / 3;
+
+            if (skaiciusFloat == skaiciusDouble)
+            {
+                Console.WriteLine("skaiciusFloat {0} YRA skaicius {1}", skaiciusFloat, skaiciusDouble);
+            }
+            else 
+            {
+                Console.WriteLine("skaiciusFloat {0} NERA skaicius {1}", skaiciusFloat, skaiciusDouble);
+            }
+            Console.ReadLine();
+        }
+    }
+}
