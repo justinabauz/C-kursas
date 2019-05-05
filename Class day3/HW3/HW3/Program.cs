d0f6d144765e1571d540a16b590a2d435802e070
Justina <justina.bauziene@gmail.com>
2019-05-05 18:15:06 +0300

    +

---
+++ Class day3/HW3/HW3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu:");
+            int number1;
+            if (!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("Ivestas ne skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Ar ivestas skaicius yra lyginis? {0}", number1%2 ==0);
+            Console.WriteLine("Ar ivestas skaicius yra teigimas? {0}", number1>0);
+            Console.ReadLine();
+        }
+    }
+}
