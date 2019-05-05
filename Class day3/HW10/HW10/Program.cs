c94eb6bbb4491d723a02a2d6e80b02401c157dc8
Justina <justina.bauziene@gmail.com>
2019-05-05 19:46:01 +0300

    +

---
+++ Class day3/HW10/HW10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite savo metines pajamas:");
+            double income;
+            if (!double.TryParse(Console.ReadLine(), out income))
+            {
+                Console.WriteLine("Ivestas ne skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            if (income < 40000)
+            {
+                Console.WriteLine("Mokestis netaikomas");
+                Console.ReadLine();
+            }
+            else if (income >= 40000 && income < 75000)
+            {
+                Console.WriteLine("Taikomas 15 % mokestis: {0}", income*0.15);
+                Console.ReadLine();
+            }
+            else if (income >= 75000 && income < 100000)
+            {
+                Console.WriteLine("Taikomas 20 % mokestis: {0}", income * 0.2);
+                Console.ReadLine();
+            }
+            else if (income > 100000)
+            {
+                Console.WriteLine("Taikomas 35 % mokestis: {0}", income * 0.35);
+                Console.ReadLine();
+            }
+        }
+    }
+}
