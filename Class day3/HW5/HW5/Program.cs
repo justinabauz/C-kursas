4a56503faa49de7434c603d687eaeb3434038bd4
Justina <justina.bauziene@gmail.com>
2019-05-05 18:43:33 +0300

    +

---
+++ Class day3/HW5/HW5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirma skaiciu:");
+            double number1;
+            if (!double.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("Ivestas ne skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Iveskite antra skaiciu:");
+            double number2;
+            if (!double.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("Ivestas ne skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Iveskite trecia skaiciu:");
+            double number3;
+            if (!double.TryParse(Console.ReadLine(), out number3))
+            {
+                Console.WriteLine("Ivestas ne skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Didziausias is siu skaiciu yra: {0}", Math.Max(Math.Max(number1, number2), number3));
+            Console.ReadLine();
+        }
+    }
+}
