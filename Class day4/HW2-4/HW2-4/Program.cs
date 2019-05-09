bdcfbea93d045ebd72d25efd4641ffc94a92e410
Justina <justina.bauziene@gmail.com>
2019-05-09 20:52:09 +0300

    +

---
+++ Class day4/HW2-4/HW2-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW24
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] masyvas = { "a", "b", "c", "d" };
+
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                Console.Write("{0}, ", masyvas[i]);
+            }
+            Console.WriteLine();
+            for (int j = masyvas.Length - 1; j >= 0; j--)
+            {
+                Console.Write("{0}, ", masyvas[j]);
+            }
+
+        }
+    }
+}
