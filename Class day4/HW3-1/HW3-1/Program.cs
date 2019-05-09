c10e2faccbacb49fd4f6dda4de751480a1fe2dcf
Justina <justina.bauziene@gmail.com>
2019-05-09 22:35:08 +0300

    +

---
+++ Class day4/HW3-1/HW3-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW31
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] masyvas = { "Beyonce (m)", "David Bowie (v)", "Elvis Costello(v)", "Madonna(m)", "Elton John(v)", "Charles Aznavour(v)" };
+
+            Console.WriteLine("Atspausdiname vyrus:");
+            Console.WriteLine();
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                if (masyvas[i].EndsWith("(v)"))
+                {
+                    Console.WriteLine(masyvas[i]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Atspausdiname moteris:");
+            Console.WriteLine();
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                if (masyvas[i].EndsWith("(m)"))
+                {
+                    Console.WriteLine(masyvas[i]);
+                }
+            }
+        }
+    }
+}
