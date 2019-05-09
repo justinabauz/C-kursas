04d4ff92a543a5f43efb88ab0923d1f26205bfdc
Justina <justina.bauziene@gmail.com>
2019-05-09 19:50:48 +0300

    +

---
+++ Class day4/HW2/HW2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
