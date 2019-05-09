c0c61d324aa621b34a08231b207db9062efbff89
Justina <justina.bauziene@gmail.com>
2019-05-09 19:47:57 +0300

    +

--- Class day4/CWM7/CWM7/Program.cs
+++ Class day4/CWM7/CWM7/Program.cs
@@ -6,7 +6,20 @@ namespace CWM7
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] numbers1 = new int[10];
+            int[] numbers1 = { 2, 5, 4, 7, 2, 8, 5, 4, 7, 9 };
+            /*Random rnd = new Random();
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                numbers1[i] = rnd.Next(1, 100);
+            }
+            */
+
+            foreach (var number in numbers1)
+            {
+                Console.Write("{0}, ", number);
+            }
+
         }
     }
 }
---
+++ Class day4/HW1/HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Justina");
+
+            }
+        }
+    }
+}
