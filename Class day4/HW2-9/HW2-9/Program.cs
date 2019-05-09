01c9630cad6ed05ad622777003e52be68c8e17fa
Justina <justina.bauziene@gmail.com>
2019-05-09 21:44:44 +0300

    +

---
+++ Class day4/HW2-9/HW2-9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW29
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] masyvas1 = { 1, 3, 9 };
+            int[] masyvas2 = { 4, 6, 7 };
+            int[] masyvas3 = new int[6];
+            for (int i=0; i<3; i++)
+            {
+                masyvas3[i] = masyvas1[i];
+            }
+
+            for (int j = 0; j < 3; j++)
+            {
+                masyvas3[3+j] = masyvas2[j];
+            }
+            foreach (int number in masyvas3)
+            {
+                Console.Write("{0}, ", number);
+            }
+
+        }
+    }
+}
