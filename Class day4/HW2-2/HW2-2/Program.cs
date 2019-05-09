c722875cdcfc8604a79e0d1408f2e38e5db4e39f
Justina <justina.bauziene@gmail.com>
2019-05-09 20:34:28 +0300

    +

---
+++ Class day4/HW2-2/HW2-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW22
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers1 = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                numbers1[i] = rnd.Next(1, 100);
+            }
+            foreach (var number in numbers1)
+            {
+                Console.Write("{0}, ", number);
+            }
+        }
+    }
+}
