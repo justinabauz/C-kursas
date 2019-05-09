d47f4b3d9279925283a05c5accf9a985a15017a4
Justina <justina.bauziene@gmail.com>
2019-05-09 22:07:11 +0300

    +

---
+++ Class day4/HW2-10/HW2-10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW210
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] varduMas = { "Justas", "Tomas", "Rokas", "Lukas", "Vaidas", "Povilas", "Martynas", "Jokubas", "Viktoras", "Benas" };
+            string[] masyvasNaujas = new string[8];
+            for (int i = 0; i<10; i++)
+            {
+                if (i < 4)
+                {
+                    masyvasNaujas[i] = varduMas[i];
+                }
+                else if (i == 5)
+                {
+                    masyvasNaujas[i-1] = varduMas[i];
+                }
+                else if (i > 6)
+                {
+                    masyvasNaujas[i-2] = varduMas[i];
+                }
+            }
+            foreach(var number1 in masyvasNaujas)
+           {
+           Console.WriteLine("{0}, ", number1);
+           }
+
+
+        }
+    }
+}
