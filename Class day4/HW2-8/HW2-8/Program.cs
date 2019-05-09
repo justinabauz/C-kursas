627c020824e1598fde0cf46dfd54525408d9e841
Justina <justina.bauziene@gmail.com>
2019-05-09 21:38:10 +0300

    +

---
+++ Class day4/HW2-8/HW2-8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW28
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] masyvas = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Iveskite skaiciu:");
+                if (!int.TryParse(Console.ReadLine(), out masyvas[i]))
+                {
+                    Console.WriteLine("Ivestas ne skaicius");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+            Array.Sort(masyvas);
+            foreach (int number in masyvas)
+            {
+                Console.Write("{0}, ", number);
+            }
+
+        }
+    }
+}
