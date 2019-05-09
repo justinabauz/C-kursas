7f9b96ec5b5ef6e9e5fc5d57a0f4c21276e02616
Justina <justina.bauziene@gmail.com>
2019-05-09 19:48:21 +0300

    +

--- Class day4/HW1/HW1/Program.cs
+++ Class day4/HW1/HW1/Program.cs
@@ -6,11 +6,9 @@ namespace HW1
     {
         public static void Main(string[] args)
         {
-
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine("Justina");
-
             }
         }
     }
