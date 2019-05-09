a915ac2b2f773e64a2d77984f6cfe746e0653314
Justina <justina.bauziene@gmail.com>
2019-05-09 19:54:13 +0300

    +

---
+++ Class day4/HW3/HW3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 10; i <= 20; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Skaiciu nuo 10 iki 20 suma: {0}", sum);
+        }
+    }
+}
