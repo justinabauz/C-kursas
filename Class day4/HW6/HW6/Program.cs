a405c449cc2fc36321bc02a756031b7cf1c1ad71
Justina <justina.bauziene@gmail.com>
2019-05-09 20:19:16 +0300

    +

---
+++ Class day4/HW6/HW6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i=1; i<=10; i++)
+            { 
+             for(int j=1; j<=10; j++)
+                {
+                    Console.Write("{0}x{1}={2}, ", i, j, i*j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
