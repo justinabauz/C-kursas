c83e6d4f3c4325bbe944be896995bc90efb8ccfe
Justina <justina.bauziene@gmail.com>
2019-05-09 20:02:22 +0300

    +

---
+++ Class day4/HW4/HW4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Skaicius dalinasi is 3:");
+            for (int i = 1; i <= 100; i++)
+            { 
+            if (i % 3 == 0)
+                {
+                    Console.Write("{0},", i);
+                }
+            }
+        }
+    }
+}
