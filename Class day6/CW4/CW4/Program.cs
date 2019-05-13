cc70dab61f9d3f838e7dd2a4f269a9c685d178e4
Justina <justina.bauziene@gmail.com>
2019-05-13 21:02:15 +0300

    +

---
+++ Class day6/CW4/CW4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CW4
+{
+    class MainClass
+    {
+        public static int Factorial(int m)
+
+        {
+            int fact = 1;
+            for (int i=m; i>0; i--)
+            {
+                fact = fact * i;
+                        }
+            return fact;
+        }
+
+        public static void Main(string[] args)
+        
+        {
+
+            int skaicius = 5;
+            Console.WriteLine("Skaiciaus faktorialas: {0}", Factorial(skaicius));
+
+        }
+    }
+}
