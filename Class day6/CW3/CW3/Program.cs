15c3b65f776c045a6b5cb329c51a7d01cce60ec3
Justina <justina.bauziene@gmail.com>
2019-05-13 20:41:18 +0300

    +

---
+++ Class day6/CW3/CW3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CW3
+{
+    class MainClass
+    {
+
+
+        public static int SkaitmenuSuma(int num)
+
+        {
+            int sum = 0;
+            for (int n = num; n > 0; sum += n % 10, n /= 10);
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            int skaicius = 67;
+
+            Console.WriteLine("skaitmenu suma: {0}", SkaitmenuSuma(skaicius));
+
+
+        }
+    }
+}
