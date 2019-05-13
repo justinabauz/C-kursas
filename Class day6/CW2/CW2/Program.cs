ceeae2245a9bbf7b409dbf9d7a568199d8a1420d
Justina <justina.bauziene@gmail.com>
2019-05-13 20:29:12 +0300

    +

---
+++ Class day6/CW2/CW2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CW2
+{
+    class MainClass
+    {
+        public static int Fibonaciai(int n)
+        {
+            int a = 0;
+            int b = 1;
+        
+        for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return a;
+        
+        }
+
+        public static void Main(string[] args)
+        {
+            int skaicius = 15;
+
+            for (int i = 0; i < skaicius; i++)
+            {
+                Console.WriteLine(Fibonaciai(i));
+            }
+        }
+    }
+}
