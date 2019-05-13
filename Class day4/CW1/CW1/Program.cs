75b48add7a3c2834b214f18b6a2921467a4eca2e
Justina <justina.bauziene@gmail.com>
2019-05-13 20:15:27 +0300

    +

--- Class day4/CW1/CW1/Program.cs
+++ Class day4/CW1/CW1/Program.cs
@@ -9,7 +9,7 @@ namespace CW1
         {
             int sum = 0;
             int cursor = 0;
-            for (int j = 0; j <= 70; j++)
+            for (int j = 0; j < 100; j++)
             {
                 Thread.Sleep(50);
                 Console.SetCursorPosition(0, 0);
@@ -17,7 +17,7 @@ namespace CW1
                 Console.SetCursorPosition(cursor + j, 1);
                 Console.WriteLine("#");
             }
-            for (int i = 0; i <= 100; i++)
+            for (int i = 0; i < 100; i++)
             {
                 sum = sum + i;
             }
---
+++ Class day6/CW1/CW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CW1
+{
+    class MainClass
+    {
+        public static string ArPalindromas(string s)
+        {
+            string newword = "";
+            for (int i = s.Length-1; i >=0; i--)
+            {
+                newword = newword + s[i];
+            }
+            return newword;
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            string s = "abu gaiso po sia guba";
+            string b = s.Replace(" ", "");
+
+
+            Console.WriteLine(ArPalindromas(s));
+            Console.WriteLine("Ar Polindromas: {0}", ArPalindromas(s).Replace(" ", "") == b);
+
+        }
+    }
+}
