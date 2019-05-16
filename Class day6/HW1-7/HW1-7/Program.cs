aab91fb48edfa1e5ee26f4c93bff442b10d41e2a
Justina <justina.bauziene@gmail.com>
2019-05-16 22:01:40 +0300

    +

---
+++ Class day6/HW1-7/HW1-7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW17
+{
+    class MainClass
+    {
+        public static decimal metodas(decimal[] skaicius)
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < skaicius.Length; i++)
+            {
+                sum = sum + Math.Round(skaicius[i],2);
+            }
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+            decimal[] skaicius = { 35.2m, 55.345m};
+            Console.WriteLine("Skaiciu suma suapvalinta iki simtuju:{0}", metodas(skaicius));
+
+        }
+    }
+}
+
