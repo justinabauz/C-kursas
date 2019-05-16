c37e08696f0581b0d8597192b37557ccfa505cf9
Justina <justina.bauziene@gmail.com>
2019-05-16 22:35:18 +0300

    +

---
+++ Class day6/HW1-10/HW1-10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW110
+{
+    class MainClass
+    {
+        public static string RandomStr(int counter)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[counter];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+
+        public static void Main(string[] args)
+        { 
+            int counter = 3;
+            Console.WriteLine("Atsitiktine eilute {0}", RandomStr(counter));
+        }
+    }
+}
