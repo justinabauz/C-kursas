149fac593d3d8f6bc0a640d71d85b22f45b06a3e
Justina <justina.bauziene@gmail.com>
2019-05-16 21:24:28 +0300

    +

---
+++ Class day6/HW1-5/HW1-5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW15
+{
+    class MainClass
+    {
+        public static void remas(string eilute)
+        {
+            for(int i=0; i<eilute.Length+4; i++)
+            {
+            Console.Write("*");
+            }
+            Console.WriteLine();
+            Console.WriteLine("* {0} *", eilute);
+            for (int i = 0; i < eilute.Length + 4; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite eilute:");
+            string eilute = Console.ReadLine();
+            remas(eilute);
+        }
+    }
+}
