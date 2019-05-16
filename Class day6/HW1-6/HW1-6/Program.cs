d4f7e76a621fd646711691c7deca9de97223aad8
Justina <justina.bauziene@gmail.com>
2019-05-16 21:47:48 +0300

    +

---
+++ Class day6/HW1-6/HW1-6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW16
+{
+    class MainClass
+    {
+        public static void metodas(int[] eilute)
+        {
+            for(int i = 0; i < eilute.Length; i++)
+            {
+                if (i == eilute.Length-1)
+                {
+                    Console.Write("{0}. ", eilute[i]);
+                }
+                else if (i != eilute.Length)
+                {
+                    Console.Write("{0}: ", eilute[i]);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] stringArray = { 3, 6, 7, 8, 9 };
+            metodas(stringArray);
+        }
+    }
+}
