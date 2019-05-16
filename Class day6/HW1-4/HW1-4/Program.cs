9ffbbf435053ae884230ca0fd2f6cd32935a808f
Justina <justina.bauziene@gmail.com>
2019-05-16 21:02:08 +0300

    +

---
+++ Class day6/HW1-4/HW1-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW14
+{
+    class MainClass
+    {
+        public static int LenghtCalc(string eilute)
+        {
+        int lenght = 0;
+        foreach(var letter in eilute)
+            {
+                lenght++;
+            }
+            return lenght;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite raidziu eilute:");
+            string value = Console.ReadLine();
+            Console.WriteLine("Sios eilutes ilgis yra:{0}", LenghtCalc(value));
+        }
+    }
+}
---
+++ Class day6/HW3/HW3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW3
+{
+    class MainClass
+    { 
+            public static string Ceasar(string value, string key)
+            {
+                char[] buffer = value.ToCharArray();
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    char letter = buffer[i];
+                    letter = (char)(letter - shift);
+                    if (letter > 'z')
+                    {
+                        letter = (char)(letter - 26);
+                    }
+                    else if (letter < 'a')
+                    {
+                        letter = (char)(letter + 26);
+                    }
+                    buffer[i] = letter;
+                }
+                return new string(buffer);
+
+            }
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Iveskite eilute");
+                string letter1 = Console.ReadLine();
+                Console.WriteLine("Iveskite rakto eilute");
+                string letter2 = Console.ReadLine();
+
+            Console.WriteLine(Ceasar(letter1, letter2));
+            }
+        }
+    }
+}
+}
